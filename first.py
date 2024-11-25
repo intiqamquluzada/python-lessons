a28020842837a8049020547ab2176c82e859939f
Intigam Guluzade <quluzadintiqam@gmail.com>
2024-11-26 00:38:35 +0400

    lesson2

---
+++ first.py
@@ -0,0 +1,36 @@
+# print("555" + 2)
+# box_1 = "555"
+# box_2 = "333"
+# print(box_1, ",", box_2)
+
+# print('Salam')
+
+# a = 20
+#
+# print(a - 12) # 8
+
+
+# b = "34"  # type str
+#
+# print(type(b))
+
+# a,b, box_1
+
+# 2box = "333" -> reqemle baslayan deyisen adi olmaz
+
+# @,-, *, _
+# my_box = 'Salam'
+
+# my-box = 'Salam1' # simvollardan yalniz altdan xett istifade etmek olar
+
+
+# CASE
+
+# my_box -> Snake case
+# myBox -> Camel case
+# MyBox -> Pascal case
+
+
+# x = "Intigam"
+# x = 'Intigam'
+
---
+++ second.py
@@ -0,0 +1,108 @@
+# a = 4
+# print(type(a))
+# b = 3.5
+# print(type(b))
+#
+# c = 'salam'
+# print(type(c)) # float
+
+# my_var2 = 'salam'
+
+# my_word = "Yasin"
+# my_word_2 = 'Yasin'
+
+"""
+SECOND LESSON
+"""
+# Casting
+# x = 5.8 # float
+#
+# y = int(x)
+# print(y)
+
+
+# x = 5 # integer
+# y = str(x)  # "5"
+
+# a = "3"
+# b = float(a)
+# print(b)
+
+# a = "A3"
+# b = float(a)
+# print(b)
+
+# Strings
+
+# my_string = """
+# salam
+# necesen
+# salam
+# """
+# print(my_string)
+
+# my_string = "Hello World!"
+# print(my_string[0:5:2])
+
+
+# my_word = "Yasin"
+# print(len(my_word))  # length = uzunluq
+
+# my_word = input("Enter your name: ")
+# print(my_word)
+
+# my_age = int(input("Enter your age: ")) # "23"
+# print(my_age + 3) # 26
+# print(my_age + 2) # 25
+
+
+# my_number = 3
+# # my_number = my_number + 2 # 5
+# # my_number = my_number * 3 # 15
+# my_number += 2 # my_number = my_number + 2
+
+# print(my_number)
+
+
+# my_word = "intigam"
+#
+# new_word = my_word.upper() # lower
+#
+# print(new_word)
+
+# name
+# surname
+
+# Intigam Guluzade
+
+# name = input("Enter your name: ")
+# surname = input("Enter your surname: ")
+#
+# full_name = name + " " + surname
+
+# print(full_name)
+
+# age = 17
+# # my_text = "Yasin is " + age # Yasin is 17
+# my_text = f"Yasin is {age}"
+# print(my_text)
+
+# my_text = 'Menim en sevdiyim serial "Sherlock Holmes"'
+# print(my_text)
+
+# name = "intigam"
+# name = name.capitalize()
+# print(name)
+"""
+1. Istifadeciden 2 eded alin ve onlarin cemini bir deyisene menimesederek ekrana cap edin.
+2. Daxil olunan sozde ilk 3 herfi cap eden python kodu yazin.
+3. Daxil edilen 2 ededin ferqlerinin, cemlerinden ne qeder boyuk oldugunu tapan python kodu yazin.
+4. Istifadeciden bir soz alin ve onun uzunlugunu ekrana cap edin.
+5. Daxil edilen sozun son herfini cap eden python kodu yazin.
+6. Daxil edilen ededin reqemlerinin sayini tapan python kodu yazin.
+7. Daxil edilen ikireqemli ededin reqemleri cemini tapan python kodu yazin.
+8. Daxil edilen sozde tek yerde duran herfleri cap eden python kodu yazin.
+9. Daxil edilen sozde cut yerde duran herfleri cap eden python kodu yazin.
+10. Daxil edilen sozun ilk 3 herfi ile son herfini birlesdirib, yeni bir deyisene menimsedib, ekrana cap edin.
+11. Uzunlugu tek sayda olan, daxil edilen sozun ortada duran herfini ekrana cap edin.
+"""
