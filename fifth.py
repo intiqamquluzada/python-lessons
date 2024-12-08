661b7963585ca3c0248988ae2976a4103958dc58
Intigam Guluzade <quluzadintiqam@gmail.com>
2024-12-08 13:41:21 +0400

    fifth

---
+++ fifth.py
@@ -0,0 +1,105 @@
+# number = int(input("Enter number: ")) # 46
+# # 3 e bolunmesinden alinan qaliq = 1
+# """
+# 46 / 3 = 15 q.1
+# """
+# qaliq = number % 3
+#
+# if qaliq % 2 == 0:
+#     print("cutdur")
+# else:
+#    print("tekdir")
+
+
+# 5.
+
+# word = input("Enter your word: ")
+#
+# if word[0] == word[-1]:
+#     print("Birinci ve axirinci herfler eynidir")
+# else:
+#     print("Eyni deyiller")
+
+
+# 2. Istifadeciden 5 eded heyvan adi alin, sertlerinize uygun
+# olaraq adlarini cap edin, eger bilmirsinizse, else halina
+# atin
+
+# animal_1 = input("Animal: ")
+# animal_2 = input("Animal: ")
+# animal_3 = input("Animal: ")
+# animal_4 = input("Animal: ")
+# animal_5 = input("Animal: ")
+#
+#
+# if animal_1 == "lion":
+#     print(animal_1)
+# if animal_2 == "cat":
+#     print(animal_2)
+# if animal_3 == "dog":
+#     print(animal_3)
+# if animal_4 == "elephant":
+#     print(animal_4)
+# if animal_5 == "snake":
+#     print(animal_5)
+
+
+# 6. Istifadecinin daxil etdiyi 3 reqemli ededin
+# reqemlerinden cut olanlari cap eden python kodu yazin.
+
+# number = input("Enter your number: ")
+#
+# first_digit = int(number[0])
+# second_digit = int(number[1])
+# third_digit = int(number[2])
+#
+# if first_digit % 2 == 0:
+#     print(first_digit)
+#
+# if second_digit % 2 == 0:
+#     print(second_digit)
+#
+# if third_digit % 2 == 0:
+#     print(third_digit)
+
+
+# Daxil edilen 5 reqemli ededde, reqemlerden necesi cutdur
+# ekrana cap edin.
+
+# number = input("Enter your number: ")
+#
+# first_digit = int(number[0])
+# second_digit = int(number[1])
+# third_digit = int(number[2])
+# fourth_digit = int(number[3])
+# fifth_digit = int(number[4])
+#
+# count = 0 # 1 -> 2
+#
+# # 12789
+#
+# if first_digit % 2 == 0:
+#     count = count + 1
+#
+# if second_digit % 2 == 0:
+#     count = count + 1
+#
+# if third_digit % 2 == 0:
+#     count = count + 1
+#
+# if fourth_digit % 2 == 0:
+#     count = count + 1
+#
+# if fifth_digit % 2 == 0:
+#     count = count + 1
+#
+#
+# print("Cut reqemlerin sayi = ", count)
+
+
+# a = 1
+# b = 2
+# c = 3
+#
+# if a > b > c:
+#     print(c, b, a)
