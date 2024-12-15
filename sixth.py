6abeb461b3d79f19d932f65dd2ee1d2d94d10999
Intigam Guluzade <quluzadintiqam@gmail.com>
2024-12-15 23:12:22 +0400

    lesson6

---
+++ sixth.py
@@ -0,0 +1,85 @@
+# password = "1234"
+# balance = 230
+#
+# input_password = input("Zəhmət olmasa parolunuzu daxil edin: ")
+#
+# if input_password == password:
+#     input_options = input("""
+#     1. Balansiniza baxin
+#     2. Pul cekmek
+#     3. Pul yatirmaq
+#     4. Cixis
+#     """)
+#     if input_options == "1":
+#         print("Sizin balansiniz", balance)
+#     elif input_options == "2":
+#         charged_money = int(input("Meblegi daxil edin: "))
+#         if balance >= charged_money:
+#             balance -= charged_money
+#             print("Tesekkur edirik", balance)
+#         else:
+#             print("Balansinizda kifayet qeder mebleg yoxdur")
+#     elif input_options == "3":
+#         added_money = int(input("Meblegi daxil edin: "))
+#         balance += added_money
+#         print("Elave edildi, sizin balansiniz", balance)
+#     elif input_options == "4":
+#         print("Sorgunuz ucun tesekkur edirik")
+#     else:
+#         print("Yalnis emeliyyat")
+# else:
+#     print("Parolunuz yalnisdir")
+
+
+# mylist = [12, 3, 4, 5, 3, 4]
+# print(mylist)
+# # mylist.append("Salam")
+# # mylist.insert(2, "Salam")
+#
+# print(mylist)
+"""
+1. Siralidir yeni indeksleri var
+2. Tekrar qebul edir.
+3. Deyiskendir, element sile yaxud elave ede bilerik
+"""
+
+# mylist_2 = ["salam", "necesen"]
+# mylist_3 = [2, "slaam", 4.5, True]
+
+
+# mystring = "salam"
+# print(mystring[0])
+
+
+# thislist = ["apple", "banana", "cherry"]
+# tropical = ["mango", "pineapple", "papaya"]
+# thislist.extend(tropical)
+# thislist += tropical
+# print(thislist)
+
+# if "mango" not in thislist:
+#     print("Mango listimde yoxdu")
+
+
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana", "bal"]
+# thislist.sort()
+# print(thislist)
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort(reverse=True)
+# print(thislist)
+
+
+# thislist = ["banana", "Orange", "Kiwi", "cherry"]
+# thislist.reverse()
+# print(thislist)
+
+
+# mylist = [1,2,3,4]
+# new_list = mylist.copy()
+#
+# new_list.append(5)
+#
+# print(mylist)
+# print(new_list)
+
+
