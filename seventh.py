1640c475c62edcbfa349baaebe7835fb882db389
Intigam Guluzade <quluzadintiqam@gmail.com>
2024-12-21 22:06:31 +0400

    seventh

---
+++ seventh.py
@@ -0,0 +1,52 @@
+# Dovr
+
+
+# cycle = ["gas", "solid", "liquid"]
+
+# print(cycle[0])
+# print(cycle[1])
+# print(cycle[2])
+
+# for dovr operatoru
+
+# for i in cycle:
+#     print(i)
+
+
+# my_string = "Yasin Ibadov"
+#
+# for b in my_string:
+#     print(b)
+
+
+# a = 789
+# for i in str(a):
+#     print(i)
+
+
+# 1-den 10-a qeder olan ededleri cap etmek
+
+# range = araliq
+
+# for i in range(1, 11):
+#     print(i)
+
+
+my_list = ["alma", "armud", "nar"]
+a = len(my_list) # 3
+# print(a)
+
+# for i in range(len(my_list)): # 0,1,2
+#     print(my_list[i])
+#
+# for i in my_list:
+#     print(i)
+
+# my_list = []
+
+my_list = []
+
+for i in range(1,11):
+    my_list.append(i)
+
+print(my_list)
